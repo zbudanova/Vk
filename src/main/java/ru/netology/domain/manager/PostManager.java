6e7fedfae29a144cb613778f9c4d60d3e78d85a2
Zarina Budanova <zarina.nurbaeva@gmail.com>
2021-11-08 19:31:08 +0500

    Add PostManager.java

---
+++ src/main/java/ru/netology/domain/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.domain.manager;
+
+import ru.netology.domain.PostsVk;
+
+public class PostManager {
+    private PostsVk[] posts;
+
+    public PostsVk[] search() {
+        return null;
+    }
+
+    public void delete() {
+    }
+}
